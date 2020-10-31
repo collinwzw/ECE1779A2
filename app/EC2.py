5470b390e1712788fd146572530271e022f0d220
Ziwen Wang <zi.w.wang@mail.mcgill.ca>
2020-10-31 01:29:22 -0400

    implementing ec2 class

--- app/EC2.py
+++ app/EC2.py
@@ -6,149 +6,61 @@ from app import config
 from datetime import datetime, timedelta
 from operator import itemgetter
 
-'''
-    Part 1
-
-    Modify the function ec2_examples.ec2_list so that display information only
-    for instances with the selected status.  
-
-    Documentation for the EC2 filter function is available at:
-
-        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html
-
-    Search in the above URL for the string "ec2.instances.filter"
-
-    The filter function takes a very large number of options.  The key that
-    reflects the status of the instance is called "instance-state-name"
-
-'''
 
 
-@app.route('/ec2_examples', methods=['GET', 'POST'])
-# Display an HTML list of all ec2 instances
-def ec2_list():
-    status = request.form.get('filter', "")
+class EC2:
 
-    # create connection to ec2
-    ec2 = boto3.resource('ec2')
-
-    if status == "" or status == "all":
+    @staticmethod
+    def getAllInstance():
+        '''
+        get all ec2 instance from AWS
+        :return: ec2 instances list. type ec2.instancesCollection
+        '''
+        ec2 = boto3.resource('ec2')
         instances = ec2.instances.all()
-    else:  # status := pending | running | shutting-down | terminated | stopping | stopped
-        # your code starts here
-        filters = [{'Name': 'instance-state-name', 'Values': [status]}]
+        return instances
+
+    @staticmethod
+    def getInstanceByStatus(filters):
+        '''
+        get ec2 instance by status
+        :param filters:
+        :return: ec2 instances list. type ec2.instancesCollection
+        '''
+        ec2 = boto3.resource('ec2')
         instances = ec2.instances.filter(Filters=filters)
+        return instances
 
-        # your code ends here
+    @staticmethod
+    def getInstanceByID(id):
+        '''
+        get instance by ID
+        :param id:
+        :return: ec2 instance
+        '''
+        ec2 = boto3.resource('ec2')
 
-    return render_template("ec2_examples/list.html", title="EC2 Instances", instances=instances)
+        instance = ec2.Instance(id)
+        return instance
 
+    @staticmethod
+    def createInstance():
+        '''
+        create one instance of pre-defined imageID with pre-defined subnet_id
+        :return: void
+        '''
+        ec2 = boto3.resource('ec2')
+        ec2.create_instances(ImageId=config.ami_id, MinCount=1, MaxCount=1,
+                             InstanceType='t2.small', SubnetId=config.subnet_id)
 
-@app.route('/ec2_examples/<id>', methods=['GET'])
-# Display details about a specific instance.
-def ec2_view(id):
-    ec2 = boto3.resource('ec2')
+    @staticmethod
+    def getDeleteInstanceByID(id):
+        '''
+        delete instance by ID
+        :param id:
+        :return: void
+        '''
+        ec2 = boto3.resource('ec2')
 
-    instance = ec2.Instance(id)
+        ec2.instances.filter(InstanceIds=[id]).terminate()
 
-    client = boto3.client('cloudwatch')
-
-    metric_name = 'CPUUtilization'
-
-    ##    CPUUtilization, NetworkIn, NetworkOut, NetworkPacketsIn,
-    #    NetworkPacketsOut, DiskWriteBytes, DiskReadBytes, DiskWriteOps,
-    #    DiskReadOps, CPUCreditBalance, CPUCreditUsage, StatusCheckFailed,
-    #    StatusCheckFailed_Instance, StatusCheckFailed_System
-
-    namespace = 'AWS/EC2'
-    statistic = 'Average'  # could be Sum,Maximum,Minimum,SampleCount,Average
-
-    cpu = client.get_metric_statistics(
-        Period=1 * 60,
-        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
-        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
-        MetricName=metric_name,
-        Namespace=namespace,  # Unit='Percent',
-        Statistics=[statistic],
-        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
-    )
-
-    cpu_stats = []
-
-    for point in cpu['Datapoints']:
-        hour = point['Timestamp'].hour
-        minute = point['Timestamp'].minute
-        time = hour + minute / 60
-        cpu_stats.append([time, point['Average']])
-
-    cpu_stats = sorted(cpu_stats, key=itemgetter(0))
-
-    statistic = 'Sum'  # could be Sum,Maximum,Minimum,SampleCount,Average
-
-    network_in = client.get_metric_statistics(
-        Period=1 * 60,
-        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
-        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
-        MetricName='NetworkIn',
-        Namespace=namespace,  # Unit='Percent',
-        Statistics=[statistic],
-        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
-    )
-
-    net_in_stats = []
-
-    for point in network_in['Datapoints']:
-        hour = point['Timestamp'].hour
-        minute = point['Timestamp'].minute
-        time = hour + minute / 60
-        net_in_stats.append([time, point['Sum']])
-
-    net_in_stats = sorted(net_in_stats, key=itemgetter(0))
-
-    network_out = client.get_metric_statistics(
-        Period=5 * 60,
-        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
-        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
-        MetricName='NetworkOut',
-        Namespace=namespace,  # Unit='Percent',
-        Statistics=[statistic],
-        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
-    )
-
-    net_out_stats = []
-
-    for point in network_out['Datapoints']:
-        hour = point['Timestamp'].hour
-        minute = point['Timestamp'].minute
-        time = hour + minute / 60
-        net_out_stats.append([time, point['Sum']])
-
-        net_out_stats = sorted(net_out_stats, key=itemgetter(0))
-
-    return render_template("ec2_examples/view.html", title="Instance Info",
-                           instance=instance,
-                           cpu_stats=cpu_stats,
-                           net_in_stats=net_in_stats,
-                           net_out_stats=net_out_stats)
-
-
-@app.route('/ec2_examples/create', methods=['POST'])
-# Start a new EC2 instance
-def ec2_create():
-    ec2 = boto3.resource('ec2')
-
-    ec2.create_instances(ImageId=config.ami_id, MinCount=1, MaxCount=1,
-                         InstanceType='t2.small', SubnetId=config.subnet_id)
-
-    return redirect(url_for('ec2_list'))
-
-
-@app.route('/ec2_examples/delete/<id>', methods=['POST'])
-# Terminate a EC2 instance
-def ec2_destroy(id):
-    # create connection to ec2
-    ec2 = boto3.resource('ec2')
-
-    ec2.instances.filter(InstanceIds=[id]).terminate()
-
-    return redirect(url_for('ec2_list'))
--- app/View.py
+++ app/View.py
@@ -1,7 +1,10 @@
 from app import app
 from flask import render_template, redirect, url_for, request, g
-
-
+from app.EC2 import EC2
+from app import config
+from datetime import datetime, timedelta
+from operator import itemgetter
+import boto3
 @app.route('/')
 @app.route('/index')
 def index():
@@ -12,3 +15,131 @@ def index():
 
     # User is not loggedin redirect to login pa ge
     return render_template("main.html",title="Landing Page")
+
+
+@app.route('/ec2_examples', methods=['GET', 'POST'])
+# Display an HTML list of all ec2 instances
+def ec2_list():
+    status = request.form.get('filter', "")
+
+    # create connection to ec2
+
+    if status == "" or status == "all":
+        instances = EC2.getAllInstance()
+    else:  # status := pending | running | shutting-down | terminated | stopping | stopped
+        # your code starts here
+        instances = EC2.getInstanceByStatus(status)
+
+        # your code ends here
+
+    return render_template("ec2_examples/list.html", title="EC2 Instances", instances=instances)
+
+
+@app.route('/ec2_examples/<id>', methods=['GET'])
+# Display details about a specific instance.
+def ec2_view(id):
+
+    instance = EC2.getInstanceByID(id)
+
+
+    client = boto3.client('cloudwatch')
+
+    metric_name = 'CPUUtilization'
+
+    ##    CPUUtilization, NetworkIn, NetworkOut, NetworkPacketsIn,
+    #    NetworkPacketsOut, DiskWriteBytes, DiskReadBytes, DiskWriteOps,
+    #    DiskReadOps, CPUCreditBalance, CPUCreditUsage, StatusCheckFailed,
+    #    StatusCheckFailed_Instance, StatusCheckFailed_System
+
+    namespace = 'AWS/EC2'
+    statistic = 'Average'  # could be Sum,Maximum,Minimum,SampleCount,Average
+
+    cpu = client.get_metric_statistics(
+        Period=1 * 60,
+        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
+        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName=metric_name,
+        Namespace=namespace,  # Unit='Percent',
+        Statistics=[statistic],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
+    )
+
+    cpu_stats = []
+
+    for point in cpu['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute / 60
+        cpu_stats.append([time, point['Average']])
+
+    cpu_stats = sorted(cpu_stats, key=itemgetter(0))
+
+    statistic = 'Sum'  # could be Sum,Maximum,Minimum,SampleCount,Average
+
+    network_in = client.get_metric_statistics(
+        Period=1 * 60,
+        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
+        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName='NetworkIn',
+        Namespace=namespace,  # Unit='Percent',
+        Statistics=[statistic],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
+    )
+
+    net_in_stats = []
+
+    for point in network_in['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute / 60
+        net_in_stats.append([time, point['Sum']])
+
+    net_in_stats = sorted(net_in_stats, key=itemgetter(0))
+
+    network_out = client.get_metric_statistics(
+        Period=5 * 60,
+        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
+        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName='NetworkOut',
+        Namespace=namespace,  # Unit='Percent',
+        Statistics=[statistic],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
+    )
+
+    net_out_stats = []
+
+    for point in network_out['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute / 60
+        net_out_stats.append([time, point['Sum']])
+
+        net_out_stats = sorted(net_out_stats, key=itemgetter(0))
+
+    return render_template("ec2_examples/view.html", title="Instance Info",
+                           instance=instance,
+                           cpu_stats=cpu_stats,
+                           net_in_stats=net_in_stats,
+                           net_out_stats=net_out_stats)
+
+
+@app.route('/ec2_examples/create', methods=['POST'])
+# Start a new EC2 instance
+def ec2_create():
+    ec2 = boto3.resource('ec2')
+
+    ec2.create_instances(ImageId=config.ami_id, MinCount=1, MaxCount=1,
+                         InstanceType='t2.small', SubnetId=config.subnet_id)
+
+    return redirect(url_for('ec2_list'))
+
+
+@app.route('/ec2_examples/delete/<id>', methods=['POST'])
+# Terminate a EC2 instance
+def ec2_destroy(id):
+    # create connection to ec2
+    ec2 = boto3.resource('ec2')
+
+    ec2.instances.filter(InstanceIds=[id]).terminate()
+
+    return redirect(url_for('ec2_list'))
